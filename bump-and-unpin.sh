cf9bf7ddd04af75c3cb9d6be8818b154fae0eceb
Jim Clark <jim@atomist.com>
2021-06-02 08:49:32 -0700

    bump

--- bump-and-unpin.sh
+++ bump-and-unpin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
-atm-bb bump-version src/atomist/main.clj
-atm-bb unpin docker/Dockerfile
+atm-bb bump-version $(pwd)/src/atomist/main.clj
+atm-bb unpin $(pwd)/docker/Dockerfile
 git commit -am 'bump'
 git push origin main
 
--- src/atomist/main.clj
+++ src/atomist/main.clj
@@ -5,7 +5,7 @@
 (defn handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (-> {:version 95}
+   :body (-> {:version 96}
              (str))})
 
 (defn -main [& args]
